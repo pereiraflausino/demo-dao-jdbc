a2034bd8e2d252bfa1ef33a10afc9ae04fe24b62
pereiraflausino <pereiraflausino@gmail.com>
2020-05-30 20:01:02 -0300

    Seller entities class

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,6 +1,9 @@
 package application;
 
+import java.util.Date;
+
 import model.entities.Department;
+import model.entities.Seller;
 
 public class Program {
 
@@ -9,7 +12,9 @@ public class Program {
 
 		Department obj = new Department(1, "book");
 		System.out.println(obj);
-
+		
+		Seller seller = new Seller(21, "PAulo", "pereira@gmail", new Date(), 3000.0, obj);
+		System.out.println(seller);
 	}
 
 }
